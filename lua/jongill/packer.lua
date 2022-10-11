ad69cfd2779a3203519ad92f639416466cdb9706
Jon Gillman <jgillman@lasermech.com>
2022-10-11 12:21:06 -0400

    added snippits and completion

--- lua/jongill/packer.lua
+++ lua/jongill/packer.lua
@@ -19,6 +19,11 @@ return require('packer').startup(function(use)
 
     use 'ThePrimeagen/vim-be-good'
 
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
 end)
 
 
